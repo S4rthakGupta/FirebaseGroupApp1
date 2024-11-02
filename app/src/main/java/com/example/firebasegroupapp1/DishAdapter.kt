a5f5ee66d741cc8dc91fb07134cb7a2b50036265
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-11-02 00:22:40 -0400

    Update DishAdapter.kt

--- app/src/main/java/com/example/firebasegroupapp1/DishAdapter.kt
+++ app/src/main/java/com/example/firebasegroupapp1/DishAdapter.kt
@@ -1,44 +1,10 @@
 package com.example.firebasegroupapp1
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
-import com.google.firebase.storage.FirebaseStorage
 
-class DishAdapter {
+class DishAdapter(options: FirebaseRecyclerOptions<Dish>) :
+    FirebaseRecyclerAdapter<Dish, DishAdapter.MyViewHolder>(options) {
 }
 
 
-
-class PhoneAdapter(options: FirebaseRecyclerOptions<Phone>) :
-    FirebaseRecyclerAdapter<Phone, PhoneAdapter.MyViewHolder>(options) {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        return MyViewHolder(inflater, parent)
-    }
-
-    override fun onBindViewHolder(holder: PhoneAdapter.MyViewHolder, position: Int, model: Phone) {
-        holder.txtBrand.text = model.Brand;
-        holder.txtPrice.text = model.Price.toString();
-        val theImage : String = model.Photo;
-
-        var storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(theImage)
-        Glide.with(holder.imgPhoto.context)
-            .load(storageReference)
-            .into(holder.imgPhoto)
-    }
-
-    class MyViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
-        RecyclerView.ViewHolder(inflater.inflate(R.layout.list_item, parent, false)) {
-        val txtBrand: TextView = itemView.findViewById(R.id.txtBrand);
-        val txtPrice: TextView = itemView.findViewById(R.id.txtPrice);
-        val imgPhoto: ImageView = itemView.findViewById<ImageView>(R.id.imgPhoto);
-
-    }
-}
