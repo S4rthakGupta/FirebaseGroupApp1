c62fa23b1335a247b583fd230cd2d1791b29bf68
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-11-02 00:37:27 -0400

    update

--- app/src/main/java/com/example/firebasegroupapp1/DishAdapter.kt
+++ app/src/main/java/com/example/firebasegroupapp1/DishAdapter.kt
@@ -1,10 +1,41 @@
 package com.example.firebasegroupapp1
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.storage.FirebaseStorage
 
 class DishAdapter(options: FirebaseRecyclerOptions<Dish>) :
     FirebaseRecyclerAdapter<Dish, DishAdapter.MyViewHolder>(options) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return MyViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: DishAdapter.MyViewHolder, position: Int, model: Dish) {
+        holder.productName.text = model.name;
+        holder.productPrice.text = model.price.toString();
+        val theImage : String = model.photo;
+
+        var storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(theImage)
+        Glide.with(holder.imgPhoto.context)
+            .load(storageReference)
+            .into(holder.imgPhoto)
+    }
+    class MyViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.list_item, parent, false)) {
+        val productName: TextView = itemView.findViewById(R.id.productName);
+        val productPrice: TextView = itemView.findViewById(R.id.productPrice);
+        val imgPhoto: ImageView = itemView.findViewById<ImageView>(R.id.imgPhoto);
+
+    }
+
 }
 
 
