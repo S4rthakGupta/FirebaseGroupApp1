fdfd2849b119ec72b6d9a9fcf046bcd9a20acef4
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-11-03 12:18:12 -0500

    commit

--- app/src/main/java/com/example/firebasegroupapp1/MainActivity.kt
+++ app/src/main/java/com/example/firebasegroupapp1/MainActivity.kt
@@ -1,31 +1,90 @@
+package com.example.firebasegroupapp1
+
+
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.util.Log
+import android.widget.Button
+
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firebasegroupapp1.DetailActivity
 import com.example.firebasegroupapp1.DishActivity
 import com.example.firebasegroupapp1.R
 
+import com.example.firebasegroupapp1.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // Use a simple splash screen layout with your logo
 
-        // Delay for the splash screen
-        Handler(Looper.getMainLooper()).postDelayed({
-            val currentUser = FirebaseAuth.getInstance().currentUser
-            if (currentUser == null) {
-                // If no user is signed in, go to SignUpActivity
-                startActivity(Intent(this, SignUpActivity::class.java))
-            } else {
-                // If user is signed in, go to HomeActivity or main content
-                startActivity(Intent(this, DishActivity::class.java))
-            }
-            finish() // Close the splash screen
-        }, 2000) // 2-second delay
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        val btnNext: Button = findViewById(R.id.splashBtn)
+        btnNext.setOnClickListener {
+
+//            FirebaseAuth.getInstance().signOut()
+            val i = Intent(
+                this,
+                DishActivity::class.java
+            )
+            this.startActivity(i)
+        }
+
+        if (auth.currentUser == null)
+        {
+            createSignInIntent()
+        } else {
+            Log.d("Current User", "User Email: ${auth.currentUser?.email}")
+
+        }
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        var currentUser = auth.currentUser
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        if
+                (result.resultCode == RESULT_OK) {
+            val user =
+                FirebaseAuth.getInstance().currentUser
+
+        } else {
+            createSignInIntent()
+        }
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+        val signInIntent =
+            AuthUI.getInstance().createSignInIntentBuilder().setIsSmartLockEnabled(false)
+                .setAvailableProviders(providers).build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private val signInLauncher =
+        registerForActivityResult(FirebaseAuthUIActivityResultContract())
+        { result ->
+            this.onSignInResult(result)
+        }
+
+
 }
