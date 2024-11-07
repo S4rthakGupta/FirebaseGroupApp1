fd36ce57d6c0137360796eea689c2f5fe301d4d7
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-11-07 03:30:08 -0500

    updated

--- app/src/main/java/com/example/firebasegroupapp1/CartItem.kt
+++ app/src/main/java/com/example/firebasegroupapp1/CartItem.kt
@@ -2,8 +2,8 @@ package com.example.firebasegroupapp1
 
 data class CartItem(
     var dishName: String? = null,
-    var price: Double? = null,
+    var price: Double = 0.0,
     var quantity: Int? = null,
-    var totalPrice: Double? = null,
+    var totalPrice: Double = 0.0,
     var key: String? = null
 )
